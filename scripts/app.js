bd4c4ec7694973975583411877c159328d5b20a8
Jason Thweatt <jthweatt90@gmail.com>
2022-11-28 09:05:36 -0600

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,52 @@
+const counterHeading = document.querySelector('.counter');
+const resetBtn = document.querySelector('.reset-btn');
+const stopBtn = document.querySelector('.stop-btn');
+const startBtn = document.querySelector('.start-btn');
+const configBtn = document.querySelector('.config-btn');
+
+// Config vars
+let startCount = 1000;
+let timeInterval = 1000;
+let rndMin = 0;
+let rndMax = 0;
+
+let counter = startCount;
+let interval = null;
+counterHeading.textContent = FormatNumber(counter);
+
+console.log('app loaded.');
+
+resetBtn.addEventListener('click', () => {
+  // console.log('reset working.');
+  counter = startCount;
+  counterHeading.textContent = FormatNumber(counter);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+});
+
+startBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  interval = window.setInterval(() => {
+    if (counter > 1400000000) {
+      counter = startCount;
+    }
+    counter += GetRandom(100, 9999999);
+    counterHeading.textContent = FormatNumber(counter);
+  }, timeInterval);
+});
+
+configBtn.addEventListener('click', () => {
+  prompt('Configuration');
+});
+
+function GetRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function FormatNumber(rawNumber) {
+  nfObject = new Intl.NumberFormat('en-US');
+  let formattedNumber = nfObject.format(rawNumber);
+  return formattedNumber;
+}
